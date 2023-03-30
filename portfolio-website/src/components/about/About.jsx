ad4eacde7c1dd532026dc5d31efe7aabd85961e8
dinesh <silvershine.zero@gmail.com>
2023-03-30 23:45:55 +0530

    add about section

--- portfolio-website/src/components/about/About.jsx
+++ portfolio-website/src/components/about/About.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/pic.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 function About() {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about_card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro enim
+            laboriosam fugit ex rerum voluptas exercitationem. Tenetur quae eius
+            minus odit eveniet soluta quo magni ratione? Nisi aperiam ut sit!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
